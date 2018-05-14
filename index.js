4e02dcd9576dd29500eb71a83faea0c0f4ed3fd5
Lior Rabin <lior@colu.com>
2018-05-14 13:49:22 +0300

    test

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const path = require('path')
 
 const init = function (osseus) {
-  this.osseus = osseus
   return new Promise((resolve, reject) => {
     const router = require(path.join(__dirname, '/lib/router'))(osseus.config)
     Object.assign(this, router)
